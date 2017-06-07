2e29b6c796e2fbffc481d99abb1e6068f0794381
ryan10328 <ryan10328@gmail.com>
2017-06-08 03:01:37 +0800

    實作 clearCompleted 方法，透過 forkJoin 併發 Delete Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Http, Response } from "@angular/http";
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/rx';
 
 @Component({
   selector: 'app-root',
@@ -6,32 +9,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isToggle: boolean = false;
 
+  constructor(private http: Http) {
+
+    this.getTodos().subscribe(x => this.todos = x);
+
+  }
+
+  isToggle: boolean = false;
   selectedType: string = 'all';
   todo: string = '';
   todos: any[] = [];
   title = 'app';
   inputHint: string = 'What needs to be done?!!!';
 
+  getTodos() {
+    return this.http.get('http://localhost:3000/todos')
+      .map(x => x.json());
+  }
 
   addTodo() {
-
     if (this.todo) {
+      this.http.post('http://localhost:3000/todos', { done: false, todo: this.todo })
+        .map((response) => response.json())
+        .concatMap(x => this.getTodos())
+        .subscribe(x => this.todos = x);
+
       // this.todos = [...this.todos, this.todo];
-      this.todos = [...this.todos, { done: false, todo: this.todo }];
+      // this.todos = [...this.todos, { done: false, todo: this.todo }];
       // let input = evt.target as HTMLInputElement;
       // this.todos = [...this.todos, input.value]; // spread syntax
       // input.value = '';
+      this.todo = '';
     }
-
-    this.todo = '';
   }
 
   clearCompleted() {
-    this.todos = this.todos.filter((item) => {
-      return !item.done;
+    let todos = this.todos.filter((item) => {
+      return item.done;
     });
+
+    let deleteObservables: any[] = [];
+    todos.forEach(item => {
+      let deleteObs = this.http.delete(`http://localhost:3000/todos/${item.id}`);
+      deleteObservables.push(deleteObs);
+    });
+
+    // forkJoin ==> 把所有的 Observable 接起來之後併發
+    Observable.forkJoin(deleteObservables)
+      .concatMap(x => this.getTodos()) // 再接 getTodos() 把最後的資料拿回來
+      .subscribe(x => this.todos = x);
   }
 
   selectChange(evt) {
